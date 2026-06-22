4072a81932e09f114142d1b1f65fe20daaf9060f
Andrei Grigorescu <grigoanr@amazon.de>
2026-06-22 18:54:57 +0200

    append for DLL

--- DLL/dll.py
+++ DLL/dll.py
@@ -16,3 +16,15 @@ class DoublyLinkedList:
         while temp is not None:
             print(temp.value)
             temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.tail = new_node
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
