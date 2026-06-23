82c5c3290b161523bd9dd5c18b46d92069523d77
Andrei Grigorescu <andrei.grigorescu2017@gmail.com>
2026-06-23 19:35:44 +0200

    Pop for stack

--- stacks/stack.py
+++ stacks/stack.py
@@ -18,3 +18,12 @@ class Stack:
             new_node.next = self.top
             self.top = new_node
         self.height += 1
+
+    def pop(self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp
