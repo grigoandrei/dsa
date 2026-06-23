3517bbd7e850770d5033fdde2e0cd7e7806511c2
Andrei Grigorescu <andrei.grigorescu2017@gmail.com>
2026-06-23 19:33:24 +0200

    stack push

--- stacks/stack.py
+++ stacks/stack.py
@@ -9,3 +9,12 @@ class Stack:
         self.top = new_node
         self.bottom = new_node
         self.height = 1
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
